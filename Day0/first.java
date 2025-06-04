e7953cbe81196eba92015fb17943da999b14e23c
Mohit Bisht <imohiitsingh@gmail.com>
2025-06-04 20:50:20 +0530

    Day0

---
+++ Day0/first.java
@@ -0,0 +1,12 @@
+package Day0;
+
+public class first { 
+    public static void main(String[] args) {
+        // logic
+        String str = "Mohit";
+        System.out.println(str);
+        str = "Mohit bisht";
+        System.out.println(str);
+        System.out.println("Hello this is out 0 class!");
+    }
+}
